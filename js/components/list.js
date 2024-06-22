434c7ac3c809479da13cf7c56d78f9af91c56631
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-22 18:24:32 -0500

    feat: :sparkles: Componente de generacion de contenido de la lista de tareas

---
+++ js/components/list.js
@@ -0,0 +1,19 @@
+export const taskList = (res) =>{
+    let plantilla = "";
+    res.forEach(element => {
+        plantilla +=/*html*/`
+        <li class="tasks" data-id="1">
+            <span class="todo__item">${element.task} </span>
+            <div class="buttons">
+                <a href="" onclick="return false;" class="check-btn">
+                    <img src="storage/img/check-svgrepo-com.svg" alt="check">
+                </a>
+                <a href="" onclick="return false;" class="delete-btn">
+                    <img src="storage/img/trash-svgrepo-com.svg" alt="trash">
+                </a>
+            </div>
+        </li>
+        `;
+    });
+    return plantilla;
+}
